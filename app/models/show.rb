831f4e1444eee538db71232ab38933013c1ea461
kimathinjoki <dfkimathinjoki@gmail.com>
2023-02-24 14:11:52 -0800

    Completed assignment

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,23 @@
 class Show < ActiveRecord::Base
-  
+    
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    # def characters
+    #     self.characters
+    # end
+
+    # def network
+    #     self.network
+    # end
+
+    def actors_list
+        self.actors.map do |actor|
+         "#{actor.first_name} #{actor.last_name}"
+        end
+    end
+
+
+
 end
